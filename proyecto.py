90dc665fd23d93f6bc9b333960b823888f9e1484
jesus chaparro <jesusleonardo89@gmail.com>
2025-11-21 23:12:07 -0400

    atentado

--- proyecto.py
+++ proyecto.py
@@ -2,4 +2,10 @@ x= 16
 y= 20
 def sumar(x, y):
     return x + y    
-print(sumar(x, y))  
+print(sumar(x, y))  jdfhfhfhfdhfdh
+dfghfdhfdhfdh
+fhfdhdfhfd
+hfdhfdhfdh
+hfdhfhfd
+hfhfdhdfh
+fhdhfdhfdhhfdh
